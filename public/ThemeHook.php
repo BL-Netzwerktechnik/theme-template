cd408db0c958a7f024282cc007363d2fb1cd1156
Justin René Back <j.back@jrbit.de>
2025-01-30 00:21:10 +0000

    cleanup

--- public/ThemeHook.php
+++ public/ThemeHook.php
@@ -1,18 +1,13 @@
 <?php
 
 use crisp\Controllers\EventController;
-use crisp\core\Cron;
-use crisp\core\Router;
-use crisp\core\Themes;
-use example\views\controllers\StartPageController;
-use crisp\types\RouteType;
-use example\EventSubscribers\TestEventSubscriber;
 use example\EventSubscribers\ThemeHookEventSubscriber;
 use example\EventSubscribers\ThemePageErrorEventSubscriber;
-use example\views\controllers\CronTestController;
 
-class ThemeHook {
-    public function __construct() {
+class ThemeHook
+{
+    public function __construct()
+    {
         EventController::getEventDispatcher()->addSubscriber(new ThemeHookEventSubscriber());
         EventController::getEventDispatcher()->addSubscriber(new ThemePageErrorEventSubscriber());
     }
